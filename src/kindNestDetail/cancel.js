6f6c28257199e0bc892b65187723617bb6d15d25
kei-kobayashi-3 <68242986+kei-kobayashi-3@users.noreply.github.com>
2023-03-13 20:31:51 +0900

    fixed

--- src/kindNestDetail/cancel.js
+++ src/kindNestDetail/cancel.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 
 function Cancel(props){
+
+  const [showInput, setShowInput] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('');
+
+  useEffect(() => {
+    // selectedOptionに応じて、inputの表示/非表示を更新する
+    if (selectedOption === "自由に入力してください") {
+      setShowInput(true);
+    } else {
+      setShowInput(false);
+    }
+  }, [selectedOption]);
+
+
   return(
-    <label>
-      <select>
-        {props.choice.map((op) =>
-        <option key={op.pdId}>{op.pdName}</option>)}
-      </select>
-    </label>
+    <>
+      <label>
+        <select onChange={(e) => setSelectedOption(e.target.value)}>
+          {props.choice.map((op) =>
+          <option key={op.pdId} value={op.pdName}>{op.pdName}</option>)}
+        </select>
+      </label>
+      <div>
+        {showInput && <input type="text"></input>}
+      </div>
+    </>
   );
 }
 
